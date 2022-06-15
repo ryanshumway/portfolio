3f4475f0fb5d008caf8133b1b178962c8662e90d
RyanShumway <ryanshumway@duck.com>
2022-06-15 08:04:22 -0600

    attempt to conditionally render navigation

--- portfolio-website/public/app.js
+++ portfolio-website/public/app.js
@@ -8,3 +8,22 @@ function setUpTabs() {
     });
   });
 }
+
+const footer = document.getElementById('footerID');
+const footerWidth = footer.offsetWidth;
+const navigationLinks = document.getElementById('navigationLinks');
+
+window.onload = setNavigationVisible();
+
+function setNavigationVisible() {
+  if (footerWidth > 451) {
+    console.log("Width is " + footerWidth + "px")
+    footer.style.display = 'none'
+    footer.style.opacity = 'none'
+    navigationLinks.style.display = 'flex'
+  } else if (footerWidth < 450) {
+    footer.style.display = "block"
+    footer.style.opacity = '100%'
+    navigationLinks.style.display = 'none'
+  }
+}
